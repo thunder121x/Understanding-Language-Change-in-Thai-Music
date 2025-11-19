0d0e3dee2e702c813570ab124b7accc407e2ce58
Nanphat Tongsirisukool <nanphatx@hotmail.com>
2025-11-20 00:17:45 +0700

    Feat: report

---
+++ dev/scrape.js
@@ -0,0 +1,29 @@
+require("dotenv").config();
+
+async function searchSong(query) {
+  const token = process.env.SPOTIFY_TOKEN;
+
+  if (!token) {
+    throw new Error("SPOTIFY_TOKEN environment variable is not set.");
+  }
+  const res = await fetch(
+    "https://api.spotify.com/v1/search?q=" +
+      encodeURIComponent(query) +
+      "&type=track&limit=1",
+    {
+      headers: { Authorization: "Bearer " + token },
+    }
+  );
+
+  const data = await res.json();
+  const track = data.tracks.items[0];
+
+  return {
+    track: track,
+    title: track.name,
+    artist: track.artists[0].name,
+    release_date: track.album.release_date,
+  };
+}
+
+searchSong("ชาวนากับงูเห่า").then(console.log);
